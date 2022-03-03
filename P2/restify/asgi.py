6b0295be2f1c9dafacc7074160f9b9f02a77c69f
azaria314 <azaria.bandak@mail.utoronto.ca>
2022-03-03 11:04:57 -0500

    Change project name from p2 to restify

--- P2/restify/asgi.py
+++ P2/restify/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for p2 project.
+ASGI config for restify project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'p2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restify.settings')
 
 application = get_asgi_application()
--- P2/restify/urls.py
+++ P2/restify/urls.py
@@ -1,4 +1,4 @@
-"""p2 URL Configuration
+"""restify URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
