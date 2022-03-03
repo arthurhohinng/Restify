5723d584f37bc298103c69504f6eb047b3199082
azaria314 <azaria.bandak@mail.utoronto.ca>
2022-03-03 11:47:18 -0500

    Add models for Restaurant and User

--- P2/restaurants/models.py
+++ P2/restaurants/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.db.models import CASCADE
 
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(null=False, blank=False)
+    followers = models.PositiveIntegerField(null=False, default=0)
+    likes = models.PositiveIntegerField(null=False, default=0)
+    description = models.TextField(null=True, blank=True)
+    # TODO: add owner as an AbstractUser
+    # TODO: add menu as Menu
+    address = models.CharField(max_length=50, null=False)
+    postal_code = models.CharField(max_length=6, null=False)
+    logo = models.ImageField(null=False)
+    phone_num = models.CharField(max_length=11, null=False)
+
+class Blogpost(models.Model):
+    title = models.CharField(null=False, blank=False, max_length=25)
+    image = models.ImageField(null=True, blank=True)
+    body = models.TextField(null=False)
+    # TODO: add author as AbstractUser
+    restaurant = models.RestaForeignKey(Restaurant, null=False, on_detele=CASCADE)
+    posted = models.DateTimeField(auto_now=True)
+    likes = models.PositiveIntegerField(null=False, default=0)
