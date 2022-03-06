3a105a0177c999e9bf8c250eab950d6062eb9a79
Arthur Ng <artrurng@gmail.com>
2022-03-06 01:50:04 -0500

    Add accounts models

--- P2/accounts/models.py
+++ P2/accounts/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from restaurants.models import Restaurant, Blogpost
+
+
+class User(AbstractUser):
+    avatar = models.ImageField(null=True)
+    restaurant = models.ForeignKey(to=Restaurant)
+    is_owner = models.BooleanField()
+
+
+class Notification(models.Model):
+    notifier = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Feed(models.Model):
+    user = models.ForeignKey(to=User)
+    # blogpost
+
+
+class LikesRestaurant(models.Model):
+    user = models.ForeignKey(to=User)
+    restaurant = models.ForeignKey(to=Restaurant)
+
+
+class LikesBlog(models.Model):
+    user = models.ForeignKey(to=User)
+    blogpost = models.ForeignKey(to=Blogpost)
+
+
+class Follows(models.Model):
+    user = models.ForeignKey(to=User)
+    restaurant = models.ForeignKey(to=Restaurant)
+
