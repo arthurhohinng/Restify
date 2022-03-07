c387823bb279c8fbcc339ae624f20785ed9d7202
azaria314 <azaria.bandak@mail.utoronto.ca>
2022-03-07 10:50:02 -0500

    Start search view

--- P2/restaurants/urls.py
+++ P2/restaurants/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from restaurants.views.blogposts import ListBlogposts
+from restaurants.views.searchpage import SearchView
 
 app_name = 'restaurants'
 urlpatterns = [
     path('<str:pk>/blogposts/', ListBlogposts.as_view(), name='blogposts'),
+    path('/search/', SearchView.as_view(), name='searchbar')
 ]
