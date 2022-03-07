6bfb0258801aea301dd8257872b5b0447e832288
Arthur Ng <artrurng@gmail.com>
2022-03-06 23:03:25 -0500

    Install djangorestframework-simplejwt

--- P2/restify/settings.py
+++ P2/restify/settings.py
@@ -32,7 +32,12 @@ ALLOWED_HOSTS = []
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
 }
 
 INSTALLED_APPS = [
