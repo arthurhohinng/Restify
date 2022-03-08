c2ade99c2de6d28fde5a3e2bda33c1bc5be3467b
Arthur Ng <artrurng@gmail.com>
2022-03-08 03:41:28 -0500

    Add authentication to profile views

--- P2/accounts/views/edit_profile.py
+++ P2/accounts/views/edit_profile.py
@@ -1,10 +1,12 @@
 from rest_framework.generics import UpdateAPIView, RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
 
 from accounts.serializers import UserSerializer
 
 
 class EditProfileView(RetrieveAPIView, UpdateAPIView):
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_object(self):
         return self.request.user
