30332bcfde939aedec5d837f740acf4cd863215a
azaria314 <azaria.bandak@mail.utoronto.ca>
2022-03-09 10:41:46 -0500

    Register models in admin.py files

--- P2/restaurants/admin.py
+++ P2/restaurants/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Restaurant, Blogpost, Menu, MenuItem, Comments
 
 # Register your models here.
+admin.site.register(Restaurant)
+admin.site.register(Blogpost)
+admin.site.register(Menu)
+admin.site.register(MenuItem)
+admin.site.register(Comments)
