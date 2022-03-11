52ea854e58095214bc4f7b99b8302ff991a8f6ec
Arthur Ng <artrurng@gmail.com>
2022-03-11 18:13:00 -0500

    Remove owned_restaurant field from CreateUserSerializer

--- P2/accounts/serializers.py
+++ P2/accounts/serializers.py
@@ -11,7 +11,7 @@ class UserSerializer(serializers.ModelSerializer):
 class CreateUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'password', 'first_name', 'last_name', 'email', 'avatar', 'owned_restaurant', 'phone_num']
+        fields = ['username', 'password', 'first_name', 'last_name', 'email', 'avatar', 'phone_num']
 
     def create(self, validated_data):
         user = User.objects.create_user(
@@ -21,7 +21,6 @@ class CreateUserSerializer(serializers.ModelSerializer):
             last_name=validated_data['last_name'],
             email=validated_data['email'],
             avatar=validated_data['avatar'],
-            owned_restaurant=validated_data['owned_restaurant'],
             phone_num=validated_data['phone_num']
         )
         return user
