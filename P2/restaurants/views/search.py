72c058d2bd5f1c9382f4271114bd50e4e68ad28b
azaria314 <azaria.bandak@mail.utoronto.ca>
2022-03-13 12:52:35 -0400

    Finish search endpoint

--- P2/restaurants/views/search.py
+++ P2/restaurants/views/search.py
@@ -13,11 +13,20 @@ class SearchView(generics.ListAPIView):
     def get_queryset(self):
         # First, extract the query from the URL
         search_query = self.kwargs['query']
-        print(search_query)
-
-        # Seperate query into words to search using each one
         results = Restaurant.objects.none()
 
-        # TODO: union results with menu item descriptions/names
-        return results.union(Restaurant.objects.filter(name__icontains=search_query), 
-                             Restaurant.objects.filter(name__icontains=search_query))
+        # Since we have pagination implemented on this page, yet we are also unioning querysets, SQLite gets mad about it.
+        # The code below is based off the "values_list" solution provided in the answer to this SO post, to bypass the error:
+        # https://stackoverflow.com/questions/65577792/error-order-by-not-allowed-in-subqueries-of-compound-statements-in-django-whi
+
+        matching_name = list(Restaurant.objects.filter(name__icontains=search_query).values_list('id', flat=True))
+        matching_addr = list(Restaurant.objects.filter(address__icontains=search_query).values_list('id', flat=True))
+        matching_menu = []
+
+        # Searching for Restaurants with matching menu items
+        matching_menu_items = MenuItem.objects.filter(name__icontains=search_query)
+        for item in matching_menu_items:
+            matching_menu += list(Restaurant.objects.filter(id=item.menu.owner.id).values_list('id', flat=True))
+
+        all_ids = matching_name + matching_addr + matching_menu
+        return Restaurant.objects.filter(id__in=all_ids)
