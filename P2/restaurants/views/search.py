c05e93ec8eb34dadc8527c7e02b45dd5c05327fe
azaria314 <azaria.bandak@mail.utoronto.ca>
2022-03-12 20:07:50 -0500

    Start search endpoint logic

--- P2/restaurants/views/search.py
+++ P2/restaurants/views/search.py
@@ -1,10 +1,23 @@
-from rest_framework.views import APIView
-from restaurants.models import Restaurant, Blogpost
+from rest_framework import generics
+from restaurants.models import Restaurant, MenuItem
 from restaurants.serializers import RestaurantSerializer
 
-class SearchView(APIView):
+class SearchView(generics.ListAPIView):
     """
     View for the search form, where users can search for restaurants.
+    URL's query is used to return JSON of matching restaurants.
     Authentication is not required.
     """
-    pass
+    serializer_class = RestaurantSerializer
+
+    def get_queryset(self):
+        # First, extract the query from the URL
+        search_query = self.kwargs['query']
+        print(search_query)
+
+        # Seperate query into words to search using each one
+        results = Restaurant.objects.none()
+
+        # TODO: union results with menu item descriptions/names
+        return results.union(Restaurant.objects.filter(name__icontains=search_query), 
+                             Restaurant.objects.filter(name__icontains=search_query))
