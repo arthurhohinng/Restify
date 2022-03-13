13c8493879a3f4a5cab88bfd11426757a133dc47
Rayhan <rayhanfaz7@gmail.com>
2022-03-13 19:02:51 -0400

    Added url for viewing comments

--- P2/restaurants/urls.py
+++ P2/restaurants/urls.py
@@ -3,11 +3,13 @@ from restaurants.views.blogposts import ListBlogposts
 from restaurants.views.search import SearchView
 from restaurants.views.menu import ListMenuItems
 from restaurants.views.get_contact_info import ContactInfoView
+from restaurants.views.comment import CommentView
 
 app_name = 'restaurants'
 urlpatterns = [
     path('<str:pk>/blogposts/', ListBlogposts.as_view(), name='blogposts'),
     path('<str:pk>/menu/', ListMenuItems.as_view(), name='menu'),
     path('search/<str:query>/', SearchView.as_view(), name='searchres'),
-    path('restaurant/contact', ContactInfoView.as_view, name='aboutres') # TODO: may need to change url
+    path('restaurant/contact/', ContactInfoView.as_view(), name='aboutres'), # TODO: may need to change url
+    path('restaurant/comments/', CommentView.as_view(), name='comments')
 ]
