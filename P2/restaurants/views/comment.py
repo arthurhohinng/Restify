3a8c223ac8a3b2338fb6132d32bd0c0b59869cac
Rayhan <rayhanfaz7@gmail.com>
2022-03-13 18:53:36 -0400

    Completed comment view class

--- P2/restaurants/views/comment.py
+++ P2/restaurants/views/comment.py
@@ -2,4 +2,7 @@ from rest_framework.generics import RetrieveAPIView
 from restaurants.serializers import RestaurantCommentSerializer
 
 class CommentView(RetrieveAPIView):
-    pass
+    serializer_class = RestaurantCommentSerializer
+
+    def get_object(self):
+        return self.request.comment
