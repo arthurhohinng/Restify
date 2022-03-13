56a17b5b660c019c9e901439372ba0b8d44b0a52
Rayhan <rayhanfaz7@gmail.com>
2022-03-13 17:16:51 -0400

    Added url for contact info view

--- P2/restaurants/urls.py
+++ P2/restaurants/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from restaurants.views.blogposts import ListBlogposts
 from restaurants.views.search import SearchView
 from restaurants.views.menu import ListMenuItems
+from restaurants.views.get_contact_info import ContactInfoView
 
 app_name = 'restaurants'
 urlpatterns = [
     path('<str:pk>/blogposts/', ListBlogposts.as_view(), name='blogposts'),
     path('<str:pk>/menu/', ListMenuItems.as_view(), name='menu'),
-    path('search/<str:query>/', SearchView.as_view(), name='searchres')
+    path('search/<str:query>/', SearchView.as_view(), name='searchres'),
+    path('restaurant/contact', ContactInfoView.as_view, name='aboutres') # TODO: may need to change url
 ]
