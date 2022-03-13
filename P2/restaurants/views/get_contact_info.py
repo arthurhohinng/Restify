55ca4f62bceb41d435cfd06efd7f2ea2e13e1fe1
Rayhan <rayhanfaz7@gmail.com>
2022-03-13 16:52:18 -0400

    Created skeleton code for contact info view

--- P2/restaurants/views/get_contact_info.py
+++ P2/restaurants/views/get_contact_info.py
@@ -1,2 +1,8 @@
 from rest_framework.generics import RetrieveAPIView
 from accounts.serializers import RestaurantContactInfoSerializer
+
+class ContactInfoView(RetrieveAPIView):
+    # serializers
+
+    def get_object(self):
+        pass
