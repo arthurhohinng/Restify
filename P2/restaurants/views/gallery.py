67272b0e27ecb3f67dc7a6148522c254b11eab33
Rayhan <rayhanfaz7@gmail.com>
2022-03-13 23:26:29 -0400

    Completed gallery view

--- P2/restaurants/views/gallery.py
+++ P2/restaurants/views/gallery.py
@@ -1,3 +1,11 @@
 from rest_framework import generics
 from restaurants.models import Restaurant, AbstractImage
 from restaurants.serializers import RestaurantGallerySerializer
+
+class GalleryView(generics.ListAPIView):
+    serializer_class = RestaurantGallerySerializer
+
+    def get_queryset(self):
+        requested_restaurant = Restaurant.objects.filter(id=self.kwargs['pk']).first()
+        requested_image = AbstractImage.objects.filter(restaurant=requested_restaurant).first()
+        return requested_image
