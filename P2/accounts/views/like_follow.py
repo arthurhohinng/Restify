b98b5f66e2bedc00ae65d39cfdca4d5a2e18ce1a
Arthur Ng <artrurng@gmail.com>
2022-03-14 02:12:54 -0400

    Add FollowRestaurantView

--- P2/accounts/views/like_follow.py
+++ P2/accounts/views/like_follow.py
@@ -4,8 +4,8 @@ from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from accounts.models import LikesRestaurant, RestaurantNotifications
-from accounts.serializers import LikeRestaurantSerializer
+from accounts.models import LikesRestaurant, RestaurantNotifications, Follows
+from accounts.serializers import LikeRestaurantSerializer, FollowRestaurantSerializer
 from restaurants.models import Restaurant
 
 
@@ -32,3 +32,28 @@ class LikeRestaurantView(CreateAPIView):
                                                        notifier=self.request.user)
                 return super().create(request, *args, **kwargs)
         return Response({"Already liked restaurant"})
+
+
+class FollowRestaurantView(CreateAPIView):
+    serializer_class = FollowRestaurantSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        request.data['restaurant'] = kwargs['restaurant_id']
+        request.data['user'] = self.request.user.id
+        try:
+            restaurant = Restaurant.objects.get(id=kwargs['restaurant_id'])
+        except ObjectDoesNotExist:
+            return Response({"detail": "Restaurant with id={id} does not exist".format(id=kwargs['restaurant_id'])},
+                            status=status.HTTP_400_BAD_REQUEST)
+        else:
+            try:
+                Follows.objects.get(user=self.request.user, restaurant=restaurant)
+            except ObjectDoesNotExist:
+                restaurant.followers += 1
+                restaurant.save()
+                description = "{user} started following your restaurant.".format(user=self.request.user.username)
+                RestaurantNotifications.objects.create(user=restaurant.owner, description=description,
+                                                       notifier=self.request.user)
+                return super().create(request, *args, **kwargs)
+        return Response({"Already following restaurant"})
