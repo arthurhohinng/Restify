fe4ed207f59bf78111032df590f50b5c027fcdbf
Rayhan <rayhanfaz7@gmail.com>
2022-03-13 23:00:18 -0400

    Changed view for comments to get query set

--- P2/restaurants/views/comment.py
+++ P2/restaurants/views/comment.py
@@ -1,8 +1,10 @@
 from rest_framework.generics import RetrieveAPIView
 from restaurants.serializers import RestaurantCommentSerializer
+from restaurants.models import Restaurant, Comment
 
 class CommentView(RetrieveAPIView):
     serializer_class = RestaurantCommentSerializer
 
-    def get_object(self):
-        return self.request.comment
+    def get_queryset(self):
+        requested_restaurant = Restaurant.objects.filter(id=self.kwargs['pk']).first()
+        return Comment.objects.filter(restaurant=requested_restaurant)
