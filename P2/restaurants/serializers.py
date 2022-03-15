f0e47dd8d51b6e51d0a150b378ebbc7229b13783
azaria314 <azaria.bandak@mail.utoronto.ca>
2022-03-14 21:24:17 -0400

    Finish add-restaurant endpoint

--- P2/restaurants/serializers.py
+++ P2/restaurants/serializers.py
@@ -42,3 +42,40 @@ class RestaurantGallerySerializer(serializers.ModelSerializer):
 class EditMenuSerializer(serializers.ModelSerializer):
     class Meta:
         pass
+
+class CreateRestaurantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = ['name', 'description', 'address', 'postal_code', 'logo', 'phone_num']
+
+    def create(self, validated_data):
+        # Make sure this user doesn't already have a restauraunt under their name
+        if Restaurant.objects.filter(owner=self.context['request'].user).exists():
+            raise serializers.ValidationError("You may only create up to one restaurant.")
+
+        try:
+            # Since description is not required
+            if 'description' in validated_data:
+                new_restaurant = Restaurant.objects.create(
+                    name=validated_data['name'],
+                    # To access the user creating the restaurant, we do the following:
+                    # https://stackoverflow.com/questions/30203652/how-to-get-request-user-in-django-rest-framework-serializer
+                    owner=self.context['request'].user,
+                    description=validated_data['description'],
+                    address=validated_data['address'],
+                    postal_code=validated_data['postal_code'],
+                    logo=validated_data['logo'],
+                    phone_num=validated_data['phone_num']
+                )
+            else:
+                new_restaurant = Restaurant.objects.create(
+                    name=validated_data['name'],
+                    owner=self.context['request'].user,
+                    address=validated_data['address'],
+                    postal_code=validated_data['postal_code'],
+                    logo=validated_data['logo'],
+                    phone_num=validated_data['phone_num']
+                )
+        except KeyError as e:
+            raise serializers.ValidationError({"detail": "{error} key must be stated in form data".format(error=e)})
+        return new_restaurant
