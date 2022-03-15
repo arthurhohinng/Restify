875dc0a1c659c7848f7a0bc43712c1d432b1ec4a
Rayhan <rayhanfaz7@gmail.com>
2022-03-15 00:10:50 -0400

    Completed add comment view

---
+++ P2/restaurants/views/add_comment.py
@@ -0,0 +1,24 @@
+import imp
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import status
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from restaurants.serializers import RestaurantCommentSerializer
+from restaurants.models import Restaurant, Comment
+from accounts.models import User
+
+class AddCommentView(CreateAPIView):
+    serializer_class = RestaurantCommentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        request.data['restaurant'] = kwargs['restaurant_id']
+        request.data['user'] = self.request.user.id
+        try:
+            restaurant = Restaurant.objects.get(id=kwargs['restaurant_id'])
+        except ObjectDoesNotExist:
+            return Response({"detail": "Restaurant with id={id} does not exist".format(id=kwargs['restaurant_id'])},
+                            status=status.HTTP_400_BAD_REQUEST)
+        Comment.objects.create(author=self.request.user, restuarant=restaurant)
+        return super().create(request, *args, **kwargs)
