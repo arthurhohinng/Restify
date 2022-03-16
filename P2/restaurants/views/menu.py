041e7f60f3bbc7723f09ed804221418aa89f1332
Rayhan <rayhanfaz7@gmail.com>
2022-03-15 21:13:31 -0400

    Completed view for add menu

--- P2/restaurants/views/menu.py
+++ P2/restaurants/views/menu.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
 from restaurants.models import Restaurant, Menu, MenuItem
-from restaurants.serializers import MenuItemSerializer
+from restaurants.serializers import MenuItemSerializer, CreateMenuSerializer
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
 
 class ListMenuItems(generics.ListAPIView):
     """
@@ -13,3 +15,11 @@ class ListMenuItems(generics.ListAPIView):
         requested_restaurant = Restaurant.objects.filter(id=self.kwargs['pk']).first()
         requested_menu = Menu.objects.filter(owner=requested_restaurant).first()
         return MenuItem.objects.filter(menu=requested_menu).order_by('category')
+
+class AddMenuView(CreateAPIView):
+    serializer_class = CreateMenuSerializer
+
+    def post(self, request, *args, **kwargs):
+        if self.request.user.is_owner:
+            return self.create(request, *args, **kwargs)
+        return Response({"detail": "User is not a restaurant owner"})
