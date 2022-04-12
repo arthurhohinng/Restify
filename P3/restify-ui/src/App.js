c5ede7902170bcebc517ff044f289e296e345ba1
azaria314 <azaria.bandak@mail.utoronto.ca>
2022-04-12 14:10:14 -0400

    Start search page functionality

--- P3/restify-ui/src/App.js
+++ P3/restify-ui/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import {searchContext} from "./Contexts/searchContext";
 import Footer from './components/Footer';
+import Results from "./components/Search";
 
+/**
+ * Until we turn this into the front page of the site, can just add your components here
+ * to test them out easily.
+ */
 function App() {
+  const [restaurants, setRestaurants] = useState([])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Footer></Footer>
+      <searchContext.Provider value={{restaurants, setRestaurants}}></searchContext.Provider>
+      <Results />
+      <Footer />
     </div>
   );
 }
---
+++ P3/restify-ui/src/components/Button/index.js
@@ -0,0 +1,15 @@
+// want to return an actual HTML button
+// we take in the value, calculate its style
+// from lec 11 code
+
+const Button = ({ value, update, isOperator }) => {
+    const style = !isOperator ? {backgroundColor: 'lightgray', color: 'black'} : {backgroundColor: 'orange', color: 'white'}
+    return <button
+        style={{...style, fontSize: '2em'}} // ... is special syntax to use the style dict defined above
+        onClick={() => update(value)} //update is a function we pass in
+    >
+        {value}
+    </button>
+}
+
+export default Button;
