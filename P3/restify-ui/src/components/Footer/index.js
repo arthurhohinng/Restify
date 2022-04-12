f372dc3c501607d35663849295d7ec2ce697b912
azaria314 <azaria.bandak@mail.utoronto.ca>
2022-04-12 11:15:31 -0400

    Create footer component

---
+++ P3/restify-ui/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./style.css"
+
+const Footer = () => {
+
+    // Footer adapted from https://mdbootstrap.com/docs/b4/jquery/navigation/footer/
+    return <footer class="text-center text-lg-start bg-dark">
+        <div class="text-center py-3">© 2022 Copyright<img style="margin-bottom:3px;" src="img/restify.png" width="90px"></img>
+        </div>
+    </footer>
+}
+
+export default Footer
