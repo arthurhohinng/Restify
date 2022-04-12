8960828afeb53efb2739025c36300c686532fc9f
azaria314 <azaria.bandak@mail.utoronto.ca>
2022-04-12 11:25:47 -0400

    Fix footer styling

--- P3/restify-ui/src/components/Footer/index.js
+++ P3/restify-ui/src/components/Footer/index.js
@@ -4,8 +4,8 @@ import "./style.css"
 const Footer = () => {
 
     // Footer adapted from https://mdbootstrap.com/docs/b4/jquery/navigation/footer/
-    return <footer class="text-center text-lg-start bg-dark">
-        <div class="text-center py-3">© 2022 Copyright<img style="margin-bottom:3px;" src="img/restify.png" width="90px"></img>
+    return <footer class="text-center bg-dark">
+        <div class="text-center py-3">© 2022 Copyright<img src="/assets/restify.png" alt="Restify" width="90px"></img>
         </div>
     </footer>
 }
