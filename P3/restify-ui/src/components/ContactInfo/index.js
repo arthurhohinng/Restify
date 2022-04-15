635d78260e880c227b4715453b551f3ba2c25b77
Rayhan <rayhanfaz7@gmail.com>
2022-04-15 17:50:35 -0400

    Added back function for contact info

---
+++ P3/restify-ui/src/components/ContactInfo/index.js
@@ -0,0 +1,18 @@
+
+function ContactInfo(props) {
+    return <>
+    <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+        <div class="container" id="comments-tab">
+            <br></br>
+            <h4>Our Location</h4>
+            <br></br>
+            <div id="rest-addr">
+                <span class="addr-part">Address:</span> props.address<br></br>
+                <span class="addr-part">Postal Code:</span> props.postal-code<br></br>
+                <span class="addr-part">Phone Number:</span> props.phone-num<br></br>
+            </div><br></br>
+            Feel free to leave a comment on the main page and we will get back to you!
+        </div>
+    </div>
+    </>
+}
