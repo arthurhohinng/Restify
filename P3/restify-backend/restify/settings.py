9b3f48102bbd92c328bb33ac7c8a4e1fb3c415cd
azaria314 <azaria.bandak@mail.utoronto.ca>
2022-04-16 17:41:08 -0400

    Remove Django CORS

--- P3/restify-backend/restify/settings.py
+++ P3/restify-backend/restify/settings.py
@@ -27,16 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
-
-
 # Application definition
 
 INSTALLED_APPS = [
     'rest_framework',
     'restaurants',
     'accounts',
-    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,7 +45,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
