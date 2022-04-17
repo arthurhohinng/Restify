955e7b11e62bccf5623c23259e1d02d8791f85b6
Rayhan <rayhanfaz7@gmail.com>
2022-04-17 19:02:52 -0400

    Added gallery and comment section to about page

---
+++ P3/restify-ui/src/components/About/index.js
@@ -0,0 +1,11 @@
+import Gallery from "../Gallery";
+import Comment from "../Comment";
+
+const About = () => {
+    return (<>
+        // TODO: restaurant logo, followers, likes, you own this page
+        // TODO: restaurant owner name and description
+        <Gallery />
+        <Comment />
+    </>)
+}
