4c38b93dce906a984c17e3c08eed50e1348b7254
azaria314 <azaria.bandak@mail.utoronto.ca>
2022-04-16 23:05:03 -0400

    Fix Layout render error

--- P3/restify-ui/src/components/Layout/index.js
+++ P3/restify-ui/src/components/Layout/index.js
@@ -5,8 +5,8 @@ import './style.css'
 // TODO: navbar component must be linked in here
 const Layout = () => {
     return <>
-        <div class="all-content">
-            <div class="outlet-content">
+        <div className="all-content">
+            <div className="outlet-content">
                 <Outlet />
             </div>
             <Footer />
