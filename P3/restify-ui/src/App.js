339074b1b505aff15ae53a898166103a6293bceb
Arthur Ng <artrurng@gmail.com>
2022-04-18 01:22:22 -0400

    Remove EditRestaurant

--- P3/restify-ui/src/App.js
+++ P3/restify-ui/src/App.js
@@ -2,10 +2,6 @@ import './App.css';
 import {useState} from 'react';
 import {searchContext} from './Contexts/searchContext';
 import Router from './components/Routers';
-import Footer from './components/Footer';
-import ContactInfo from "./components/ContactInfo";
-import Comment from "./components/Comment";
-import EditRestaurant from "./components/EditRestaurant";
 
 /**
  * Until we turn this into the front page of the site, can just add your components here
