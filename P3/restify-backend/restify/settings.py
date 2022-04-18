b8e388316cd703740c37694d63cd978316b81e86
azaria314 <azaria.bandak@mail.utoronto.ca>
2022-04-18 03:23:54 -0400

    Fix CORS headers...again...

--- P3/restify-backend/restify/settings.py
+++ P3/restify-backend/restify/settings.py
@@ -28,8 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'http://192.168.87.135:3000'
-    'http://127.0.0.1:3000'
+    'http://192.168.87.135:3000',
+    'http://127.0.0.1:3000',
 ]
 
 # Application definition
