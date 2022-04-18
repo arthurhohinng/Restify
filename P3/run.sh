a41cd1110bc311c67ad6a9a936d7b60e3445263d
azaria314 <azaria.bandak@mail.utoronto.ca>
2022-04-18 14:02:08 -0400

    Change default testowner

--- P3/run.sh
+++ P3/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 cd restify-ui
 npm start & python3 ../restify-backend/manage.py runserver
+
+# DEFAULT USER WHO IS AN OWNER: testowner
+# PASSWORDS FOR ALL USERS: password
+# ADMIN USER AND PASSWORD: admin, admin
