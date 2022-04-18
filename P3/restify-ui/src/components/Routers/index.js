430b49cfcd1b3796420fd850a18e753667fc2d00
Arthur Ng <artrurng@gmail.com>
2022-04-18 03:11:27 -0400

    Update route

--- P3/restify-ui/src/components/Routers/index.js
+++ P3/restify-ui/src/components/Routers/index.js
@@ -26,7 +26,6 @@ const Router = () => {
                     <Route path="profile/edit/" element={<Profile isEdit={true}/>} />
                     <Route path="restaurant/:id/" element={<RestaurantPage />} />
                     <Route path="restaurant/add-restaurant/" element={<AddRestaurant />} />
-                    <Route path="restaurant/add-blogpost/" element={<AddBlog />} />
                     <Route path="restaurant/edit-menu/" element={<AddEditMenu />} />
                     <Route path="feed/" element={<Feed />} />
                     <Route path="*" element={<PageNotFound />} />
