37d6ea49261f9c864c72a725e3f0eab26953f1aa
Arthur Ng <artrurng@gmail.com>
2022-04-18 00:54:03 -0400

    Add pagination to menu

--- P3/restify-ui/src/components/Menu/index.js
+++ P3/restify-ui/src/components/Menu/index.js
@@ -1,87 +1,75 @@
 import {useState, useEffect} from 'react';
 import API from '../API';
 import BASEURL from '../BASEURL';
-import './style.css'
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
+import './style.css';
+import Button from '../Button';
 
 // make function to get all menu data and return it in a nested json object,
 // where the key is the category, something along those lines
 const Menu = () => {
-    const [items, setItems] = useState([])
+    const [items, setItems] = useState({items:[], page:1})
+    const [nextExists, setNextExists] = useState(0)
     const [categories, setCategories] = useState([])
 
     useEffect(() => {
         const url = window.location.href
         const restaurantId = url.split("/")[4]
-        const fetchUrl = `${API}/restaurants/${restaurantId}/menu/`
         const token = JSON.parse(localStorage.getItem("token"))
-        getAllMenu(fetchUrl)
-        }, [])
-
-    const getAllMenu = (url) => {
-        fetch(`${url}`, {
+        console.log(items.page)
+        console.log("fired")
+        fetch(`${API}/restaurants/${restaurantId}/menu/?page=${items.page}`, {
             method: 'GET',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
             },
-        })
-        .then(results => {
-            if (results.status === 200)
-                return results.json()
-            // else 
-            //     window.location.href = `${BASEURL}/restaurant/${restaurantId}/`
-        })
-        .then(data => {
-            setItems(data.results)
-            return data.next
-        })
-        .then(next => {
-            console.log(next)
-            if (next == null){
-                getCategories()
-            }
-            else{
-                getAllMenu(next)
-            }
-        })
-        .catch(err => {
-            console.log("error:" + err)
-        })
-        
+            })
+            .then(results => {
+                if (results.status === 200)
+                    return results.json()
+                // else 
+                //     window.location.href = `${BASEURL}/restaurant/${restaurantId}/`
+            })
+            .then(data => {
+                setItems(data.results)
+                setNextExists(data.next)
+                const categoryList = data.results.map(item => item.category)
+                setCategories([... new Set(categoryList)])
+            })
+            .catch(err => {
+                console.log("error:" + err)
+            })
+        }, [setItems, items])
 
+
+    if (items.length > 0){
+        return ( <>
+                <h1>Menu</h1>
+                {categories.map(category => 
+                    <div key={category}> 
+
+                        <h2 className="menu-category">{category}</h2> 
+                        <table className="menu-items">
+                            <tbody>
+                            {items.filter(item => item.category == category ).map(item => 
+                                <tr key={item.id}>
+                                    <td>
+                                        {item.name}
+                                        <span className="price">{item.price}</span>
+                                        <br/>
+                                        <i>{item.description}</i>
+                                    </td>
+                                </tr>
+                            )} 
+                            </tbody>
+                        </table>
+                    </div> 
+                )} 
+                {(items.page > 1) ? <Button value="prev" update={() => setItems({...items, page: items.page - 1})} /> : <></>}
+                    {nextExists !== null ? <Button value="next" update={() => setItems({...items, page: items.page + 1})} /> : <></>}
+            </>
+        )
     }
-
-    const getCategories = () => {
-        console.log(items)
-        const categoryList = items.map(item => item.category)
-        setCategories([... new Set(categoryList)])
-    }
-    return ( <>
-            <h1>Menu</h1>
-            {categories.map(category => 
-                <div key={category}> 
-
-                    <h2 className="menu-category">{category}</h2> 
-                    <table className="menu-items">
-                        <tbody>
-                        {items.filter(item => item.category == category ).map(item => 
-                            <tr key={item.id}>
-                                <td>
-                                    {item.name}
-                                    <span className="price">{item.price}</span>
-                                    <br/>
-                                    <i>{item.description}</i>
-                                </td>
-                            </tr>
-                        )} 
-                        </tbody>
-                    </table>
-                </div> 
-            )} 
-        </>
-    )
 }
 export default Menu
 /*
