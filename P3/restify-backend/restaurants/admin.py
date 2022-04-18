a0e0ea3c7797067584a8ea17b1b97969e97cbf5c
azaria314 <azaria.bandak@mail.utoronto.ca>
2022-04-18 02:13:54 -0400

    Add gallery images

--- P3/restify-backend/restaurants/admin.py
+++ P3/restify-backend/restaurants/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Restaurant, Blogpost, Menu, MenuItem, Comment
+from .models import Restaurant, Blogpost, Menu, MenuItem, Comment, AbstractImage
 
 # Register your models here.
 admin.site.register(Restaurant)
@@ -7,3 +7,4 @@ admin.site.register(Blogpost)
 admin.site.register(Menu)
 admin.site.register(MenuItem)
 admin.site.register(Comment)
+admin.site.register(AbstractImage)
