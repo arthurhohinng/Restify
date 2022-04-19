d33483ed492c3336078beea0a186f27dbf7d86a1
Arthur Ng <artrurng@gmail.com>
2022-04-18 20:10:59 -0400

    Add AddComment component

--- P3/restify-ui/src/components/Comment/index.js
+++ P3/restify-ui/src/components/Comment/index.js
@@ -2,6 +2,7 @@ import API from '../API';
 import {useState, useEffect} from "react";
 import Button from '../Button';
 import './style.css'
+import AddComment from '../FormPages/AddComment';
 
 const Comment = () => {
     const [comments, setComments] = useState({comments:[], page:1})
@@ -35,12 +36,14 @@ const Comment = () => {
             {(comments.page > 1) ? <Button value="prev" update={() => setComments({...comments, page: comments.page - 1})} /> : <></>}
             {nextExists != null ? <Button value="next" update={() => setComments({...comments, page: comments.page + 1})} /> : <></>}
             <br></br>
+            <AddComment comments={comments} setComments={setComments}/>
         </>
     }
     else {
         return (<>
             <div><h3 className="title">Comments</h3></div>
             <h4><div style={{ textAlign: 'center'}}>No Comments yet.</div></h4>
+            <AddComment comments={comments} setComments={setComments}/>
             </>)
     }
 }
---
+++ P3/restify-ui/src/components/FormPages/AddComment/index.js
@@ -0,0 +1,60 @@
+import Input from '../../Input';
+import React, { useState } from "react";
+import "../style.css";
+import API from '../../API';
+
+const AddComment = ( {comments, setComments} ) => {
+    const [inputFields, setInputField] = useState({
+        id: '',
+        restaurant: '',
+        body: '',
+        author: ''
+    })
+
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const inputsHandler = e =>{
+        setInputField( {...inputFields, [e.target.name]: e.target.value} )
+    }
+
+    const submitHandler = () =>{
+        const token = JSON.parse(localStorage.getItem("token"))
+        const url = window.location.href
+        const restaurantId = url.split("/")[4]
+        let formData = new FormData()
+        formData.append('id', restaurantId)
+        formData.append('text', inputFields.text)
+        fetch(`${API}/restaurants/${restaurantId}/add-comment/`, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+        },
+            body: formData
+        })
+        .then(results => {
+            if (results.status === 201){
+                return results.json()
+            }
+        })
+        .then(data => {
+            setComments({...comments, comments: [...comments.comments, data]})
+        })
+        .catch(err => {
+            console.log("error: " + err)
+        })
+    }
+    
+    return (
+        <div className="commentformsubmit">
+            <h4>Got a comment?</h4>
+            <div className="form-group">
+                <textarea id="comment-box" type="textarea" name="text" onChange={inputsHandler} placeholder="Enter comment here..." rows="4" cols="50" />
+            </div>
+            <br/>
+            {errorMessage}
+            <br/>
+            <input id="add-comment-button" type="button" value="Add comment" onClick={submitHandler}/>
+        </div>
+    )
+}
+export default AddComment;
